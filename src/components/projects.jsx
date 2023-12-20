322d46f405f368ab952a7b05027d80dfb7660ded
Batuhan Ozturk <fbatuhanr@gmail.com>
2023-12-20 17:25:32 +0300

    New projects added

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -5,6 +5,14 @@ import {AiFillGithub} from "react-icons/ai";
 
 import rentacarImg from "../assets/projects/rentacar.jpg"
 import shoppingcartImg from "../assets/projects/shoppingcart.jpg"
+import photogalleryImg from "../assets/projects/photogallery.jpg"
+import profilemanagementImg from "../assets/projects/profilemanagement.jpg"
+import weatherforecastImg from "../assets/projects/weatherforecast.jpg"
+import calculatorImg from "../assets/projects/calculator.jpg"
+
+import {FaBootstrap, FaNodeJs, FaReact} from "react-icons/fa";
+import {IoLogoFirebase} from "react-icons/io5";
+import {SiAxios, SiFramer, SiMongodb} from "react-icons/si";
 
 
 const projectsData = [
@@ -12,6 +20,7 @@ const projectsData = [
         title: "Rent a Car",
         description: "Car rental website created by React",
         image: rentacarImg,
+        technologies: [<FaReact title="React"/>, <FaBootstrap title="Bootstrap" />],
         liveDemo: "https://rent-a-car-reactjs.vercel.app",
         sourceCode: "https://github.com/fbatuhanr/RentaCar-ReactJS"
     },
@@ -19,8 +28,37 @@ const projectsData = [
         title: "Shopping Cart",
         description: "Shopping Cart is E-commerce website application created by React",
         image: shoppingcartImg,
+        technologies: [<FaReact title="React"/>, <FaBootstrap title="Bootstrap"/>],
         liveDemo: "https://shopping-cart-reactjs-seven.vercel.app/",
         sourceCode: "https://github.com/fbatuhanr/shoppingcart-reactjs"
+    },
+    {
+        title: "Photo Gallery",
+        description: "A simple Photo Gallery app created with React and Google Firebase!",
+        image: photogalleryImg,
+        technologies: [<FaReact title="React"/>, <IoLogoFirebase title="Firebase"/>, <SiFramer title="Framer Motion"/>],
+        sourceCode: "https://github.com/fbatuhanr/PhotoGallery-ReactJS"
+    },
+    {
+        title: "Profile Management",
+        description: "Create your profile and customize your preferences, and browse other created profiles.",
+        image: profilemanagementImg,
+        technologies: [<FaReact title="React"/>, <FaBootstrap title="Bootstrap"/>, <FaNodeJs title="NodeJs"/>, <SiMongodb title="MongoDb"/>],
+        sourceCode: ["https://github.com/fbatuhanr/Profile-Management-Frontend", "https://github.com/fbatuhanr/Profile-Management-Backend"]
+    },
+    {
+        title: "Weather Forecast",
+        description: "Weather Forecast application created with React and by using 'openweathermap' and 'geodb-cities' apis.",
+        image: weatherforecastImg,
+        technologies: [<FaReact title="React"/>, <FaBootstrap title="Bootstrap"/>, <SiAxios title="Axios"/>],
+        sourceCode: "https://github.com/fbatuhanr/WeatherForecast-ReactJS"
+    },
+    {
+        title: "Calculator",
+        description: "This calculator simple to use and functional also it has full responsive design and it supports keyboard inputs.",
+        image: calculatorImg,
+        technologies: [<FaReact title="React"/>, <FaBootstrap title="Bootstrap"/>, <SiFramer title="Framer Motion"/>],
+        sourceCode: "https://github.com/fbatuhanr/Calculator-ReactJS"
     }
 ]
 
@@ -36,7 +74,6 @@ const Projects = () => {
                 <Row className="gx-5 justify-content-center">
                     {
                         projectsData.map(data =>
-
                             <Col xs={12} md={12} lg={9}>
                                 <Card className="overflow-hidden shadow rounded-4 border-0 mb-5">
                                     <Card.Body className="card-body">
@@ -48,13 +85,35 @@ const Projects = () => {
                                                 <p className="mt-3 mb-4">
                                                     {data.description}
                                                 </p>
+                                                {
+                                                    data.technologies &&
+                                                    (<p>
+                                                    <span>Technologies:</span>
+                                                        {
+                                                            Array.isArray(data.technologies)
+                                                                ? data.technologies.map(i => <span>{i} </span>)
+                                                                : data.technologies
+                                                        }
+                                                    </p>)
+                                                }
                                                 <div>
-                                                    <a href={data.liveDemo} target="_blank">
-                                                        <Button variant="dark"><TbWorldWww /> Live Demo</Button>
-                                                    </a>
-                                                    <a href={data.sourceCode} target="_blank">
-                                                        <Button variant="light"><AiFillGithub /> Source Code</Button>
-                                                    </a>
+                                                    {
+                                                        data.liveDemo &&
+                                                        (Array.isArray(data.liveDemo)
+                                                        ?
+                                                            data.liveDemo.map(i => <a href={i} target="_blank"><Button variant="dark"><TbWorldWww /> Live Demo</Button></a>)
+                                                        :
+                                                            <a href={data.liveDemo} target="_blank"><Button variant="dark"><TbWorldWww /> Live Demo</Button></a>)
+                                                    }
+                                                    &nbsp;
+                                                    {
+                                                        data.sourceCode &&
+                                                        (Array.isArray(data.sourceCode)
+                                                            ?
+                                                            data.sourceCode.map((i, j) => <><a href={i} target="_blank"><Button variant={data.liveDemo ? "light" : "dark"}><AiFillGithub /> Source Code {j+1}</Button></a>&nbsp;</>)
+                                                            :
+                                                            <a href={data.sourceCode} target="_blank"><Button variant={data.liveDemo ? "light" : "dark"}><AiFillGithub /> Source Code</Button></a>)
+                                                    }
                                                 </div>
                                             </Col>
                                             <Col xs={3}>
